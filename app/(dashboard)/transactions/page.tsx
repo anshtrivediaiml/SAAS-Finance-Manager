0f00ef046efebba8de4e469f7ef7e9995c969b9d
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-06-05 19:04:10 +0530

    Import CSV Functionality added and its formating done by using react-papaparseuseCSVlibrary

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -8,11 +8,35 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction';
 import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-delete-transactions';
 import { useGetTransactions } from '@/features/transactions/api/use-get-transactions';
+import { useState } from 'react';
+import { UploadButton } from './upload-button';
+import { ImportCard } from './import-card';
 
-
+enum VARIANTS{
+    LIST="LIST",
+    IMPORT="IMPORT"
+}
+const Initial_Import_Results={
+    data:[],
+    errors:[],
+    meta:{},
+}
 
 
 const TransactionsPage =()=>{
+
+    const [variant,setVariant]=useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults,setImportResults]=useState(Initial_Import_Results);
+
+    const onUpload =(results:typeof Initial_Import_Results)=>{
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    }
+
+    const onCancelImport=()=>{
+        setImportResults(Initial_Import_Results);
+        setVariant(VARIANTS.LIST);
+    }
 
     const newTransaction =useNewTransaction();
     const deleteTransaction= useBulkDeleteTransactions();
@@ -38,6 +62,15 @@ const TransactionsPage =()=>{
  )   
 }
 
+if(variant===VARIANTS.IMPORT){
+
+    return (
+       <>
+       <ImportCard data={importResults.data} onCancel={onCancelImport} onSubmit={()=>{}}/>
+       </> 
+    )
+}
+
     return (
         <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
             <Card className='border-none drop-shadow-sm'>
@@ -45,10 +78,16 @@ const TransactionsPage =()=>{
                     <CardTitle className='text-xl line-clamp-1'>
                         Transaction History
                     </CardTitle>
-                    <Button onClick={newTransaction.onOpen} size="sm">
+                    <div className='flex  flex-col lg:flex-row items-center gap-x-2 gap-y-2'>
+                    <Button onClick={newTransaction.onOpen} size="sm" className='w-full lg:w-auto'>
                         <Plus className='size-4 mr-2'/>
                         Add new
                     </Button>
+                    <UploadButton 
+                     onUpload={onUpload}
+                    />
+                    </div>
+
                 </CardHeader>
                 <CardContent>
                     <DataTable columns={columns} 
